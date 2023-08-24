afb901bb866c16d7f5618ec2d8d80475effbc866
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-08-24 16:49:56 +0200

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const input = document.querySelector("input#searchByID");
+    console.log(input.value);
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(resp => resp.json())
+      .then(movie => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+        title.innerText = movie.title;
+        summary.innerText = movie.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
